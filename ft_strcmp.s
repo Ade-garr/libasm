fa21ef1447c3cd9c748aaf72401a061271bcd18b
user42 <ade-garr@student.42.fr>
2020-10-16 14:00:25 +0200

    16/10/20_13H59

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -2,26 +2,19 @@ section .text
     global  ft_strcmp
 
 ft_strcmp:
-    xor rcx, rcx
-    mov rcx, 0
-
-_loop:
-    mov rbx, [rdi + rcx]
-    cmp rbx, [rsi + rcx]
+    mov bl, [rdi]
+    cmp bl, [rsi]
     jnz _finish
-    cmp byte[rdi + rcx], 0
-    jz  _equal
-    inc rcx
-    jmp _loop
-
-_equal:
-    mov rax, 0
-    ret
+    cmp byte[rdi], 0
+    jz  _finish
+    inc rdi
+    inc rsi
+    jmp ft_strcmp
 
 _finish:
     xor rax, rax
-    mov al, [rdi + rcx]
+    mov al, [rdi]
     xor rbx, rbx
-    mov bl, [rsi + rcx]
+    mov bl, [rsi]
     sub rax, rbx
     ret
